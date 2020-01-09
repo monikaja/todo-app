49a7da990076a3bc1c51af9b44b2fe0d172be126
monicagonzalez <monica.gonzalez@grupoonetec.com>
2020-01-09 11:15:47 +0100

    Create files to implement ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { TodoComponent } from './todo/todo.component';
-import { TodosListComponent } from './todo/todos-list/todos-list.component';
-import { TodoItemComponent } from './todo/todo-item/todo-item.component';
-import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
-import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {TodoComponent} from './todo/todo.component';
+import {TodosListComponent} from './todo/todos-list/todos-list.component';
+import {TodoItemComponent} from './todo/todo-item/todo-item.component';
+import {TodoFooterComponent} from './todo/todo-footer/todo-footer.component';
+import {TodoAddComponent} from './todo/todo-add/todo-add.component';
+import {StoreModule} from "@ngrx/store";
+import {todoReducer} from "./todo/todo.reducer";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -22,9 +26,15 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({todos: todoReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/todo/todo.reducer.ts
@@ -0,0 +1,16 @@
+import * as fromTodo from './todo.actions';
+import {Todo} from "./model/todo.model";
+
+const initialState: Todo [] = [];
+
+export function todoReducer(state = initialState,
+                            action : fromTodo.Actions){
+  switch (action.type) {
+    case fromTodo.ADD_TODO:
+      const todo = new Todo(action.text);
+      return [...state, todo];
+
+    default:
+      return state;
+  }
+}
